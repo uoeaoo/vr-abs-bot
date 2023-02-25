bcd64a33195013993c9e4c9fb1643728eec71b7b
unknown <uoeaoo@gmail.com>
2023-02-26 01:42:13 +0300

    chore(project): cofigure simple chat example.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info('ola muchacho');
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf('5606583421:AAHXMmkQTs7dVk27b9292q2-MsLjk0ULY4A');
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hello!'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
